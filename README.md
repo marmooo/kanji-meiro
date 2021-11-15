4995c39f5218b802d63f4b86dfc6cb191d617b66
marmooo <marmooo@users.noreply.github.com>
2021-11-15 11:48:29 +0900

    from ngram-idioms to graded-idioms-ja

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Installation
 
-install [marmooo/ngram-idioms](https://github.com/marmooo/ngram-idioms)
+install [marmooo/graded-idioms-ja](https://github.com/marmooo/graded-idioms-ja) licensed under the CC-BY 4.0
 
 ## Build
 
--- build.js
+++ build.js
@@ -1,41 +1,48 @@
-Deno.mkdirSync("src/data/idiom/2", { recursive: true });
-Deno.mkdirSync("src/data/idiom/3", { recursive: true });
-Deno.mkdirSync("src/data/ignored/2", { recursive: true });
-Deno.mkdirSync("src/data/ignored/3", { recursive: true });
+import { readLines } from "https://deno.land/std/io/mod.ts";
 
-for (let i = 1; i <= 10; i++) {
-  Deno.copyFile(
-    "ngram-idioms/kanji-2-10000/" + i + ".lst",
-    "src/data/idiom/2/" + i + ".lst",
-  );
-  Deno.copyFile(
-    "ngram-idioms/kanji-3-5000/" + i + ".lst",
-    "src/data/idiom/3/" + i + ".lst",
-  );
+async function build(threshold) {
+  Deno.mkdirSync("src/data/idiom/2", { recursive: true });
+  Deno.mkdirSync("src/data/idiom/3", { recursive: true });
+  Deno.mkdirSync("src/data/ignored/2", { recursive: true });
+  Deno.mkdirSync("src/data/ignored/3", { recursive: true });
+  for (let i = 1; i <= 10; i++) {
+    const idiom2 = [];
+    const idiom3 = [];
+    const ignored2 = [];
+    const ignored3 = [];
+    const fileReader = await Deno.open(
+      `graded-idioms-ja/dist/${i}.csv`,
+    );
+    for await (const line of readLines(fileReader)) {
+      const arr = line.split(",");
+      const word = arr[0];
+      const count = parseInt(arr[1]);
+      if (count >= threshold) {
+        switch (word.length) {
+          case 2:
+            idiom2.push(word);
+            break;
+          case 3:
+            idiom3.push(word);
+            break;
+        }
+      } else {
+        switch (word.length) {
+          case 2:
+            ignored2.push(word);
+            break;
+          case 3:
+            ignored3.push(word);
+            break;
+        }
+      }
+    }
+    Deno.writeTextFileSync(`src/data/idiom/2/${i}.lst`, idiom2.join("\n"));
+    Deno.writeTextFileSync(`src/data/idiom/3/${i}.lst`, idiom3.join("\n"));
+    Deno.writeTextFileSync(`src/data/ignored/2/${i}.lst`, ignored2.join("\n"));
+    Deno.writeTextFileSync(`src/data/ignored/3/${i}.lst`, ignored3.join("\n"));
+  }
 }
 
-for (let i = 1; i <= 10; i++) {
-  const problemText = await Deno.readTextFile(
-    "ngram-idioms/kanji-2-10000/" + i + ".lst",
-  );
-  const tmpText = await Deno.readTextFile(
-    "ngram-idioms/kanji-2-all/" + i + ".lst",
-  );
-  const problem = problemText.split("\n");
-  const tmp = tmpText.split("\n");
-  const ignored = tmp.slice(problem.length - 1);
-  Deno.writeTextFile("src/data/ignored/2/" + i + ".lst", ignored.join("\n"));
-}
-
-for (let i = 1; i <= 10; i++) {
-  const problemText = await Deno.readTextFile(
-    "ngram-idioms/kanji-3-5000/" + i + ".lst",
-  );
-  const tmpText = await Deno.readTextFile(
-    "ngram-idioms/kanji-3-all/" + i + ".lst",
-  );
-  const problem = problemText.split("\n");
-  const tmp = tmpText.split("\n");
-  const ignored = tmp.slice(problem.length - 1);
-  Deno.writeTextFile("src/data/ignored/3/" + i + ".lst", ignored.join("\n"));
-}
+const threshold = 100000;
+await build(threshold);
