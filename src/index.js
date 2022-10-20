499641a139c61138d6a9746fe70e86cfcb30c999
marmooo <marmooo@users.noreply.github.com>
2022-10-20 09:39:12 +0900

    role="button"

--- src/index.js
+++ src/index.js
@@ -489,6 +489,7 @@ function generateGame() {
     meiroNode.appendChild(tr);
     for (let y = 0; y < size; y++) {
       const td = document.createElement("td");
+      td.setAttribute("role", "button");
       const currPos = meiro[x][y];
       if (currPos == 0) {
         td.textContent = words[getRandomInt(0, words.length)];
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-14 09:05";
+var CACHE_NAME = "2022-10-20 09:35";
 var urlsToCache = [
   "/kanji-meiro/",
   "/kanji-meiro/index.js",
