346a4a1bdcc070a53011ec149a323fd2d781997b
marmooo <marmooo@users.noreply.github.com>
2023-07-05 08:54:39 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -62,7 +62,17 @@ loadConfig();
 
 function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+function toggleDarkMode() {
+  if (localStorage.getItem("darkMode") == 1) {
+    localStorage.setItem("darkMode", 0);
+    document.documentElement.setAttribute("data-bs-theme", "light");
+  } else {
+    localStorage.setItem("darkMode", 1);
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 }
 
@@ -524,16 +534,6 @@ function resizeFontSize(node) {
   const border = 11;
   const fontSize = (meiroSize - padding - border) / 12 / margin;
   node.style.fontSize = fontSize + "px";
-}
-
-function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    delete document.documentElement.dataset.theme;
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.dataset.theme = "dark";
-  }
 }
 
 function getProblemUrls(course) {
